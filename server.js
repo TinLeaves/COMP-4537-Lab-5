d1faef1214c78408af68b34fba06af79efba8ee5
TinLeaves <tinleaves.0@gmail.com>
2024-10-13 18:45:19 -0700

    added CORS implementation

--- server.js
+++ server.js
@@ -8,17 +8,31 @@ class API {
         }
     }
 
-    handelRequest(req, res) {
+    setCorsHeaders(res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    }
+
+    handleRequest(req, res) {
+        this.setCorsHeaders(res);
+
+        if (req.method === "OPTIONS") {
+            res.writeHead(204);
+            res.end();
+            return;
+        }
+
         let body = "";
         req.on("data", (chunk) => {
             body += chunk.toString();
         });
 
-        if (req.method == "POST") {
-            if (req.url === "/insertTestRows") {
-                database.insertTestRows(res);
-            } else if (req.url === "/sql") {
-                req.on("end", () => {
+        req.on("end", () => {
+            if (req.method === "POST") {
+                if (req.url === "/insertTestRows") {
+                    database.insertTestRows(res);
+                } else if (req.url === "/sql") {
                     const query = JSON.parse(body).query;
                     if (/^(INSERT)/i.test(query)) {
                         database.query(query, (err, results) => {
@@ -34,11 +48,9 @@ class API {
                         res.writeHead(400, { "Content-Type": "application/json" });
                         res.end(JSON.stringify({ error: "Only INSERT queries are allowed in POST requests." }));
                     }
-                });
-            }
-        } else if (req.method === "GET") {
-            if (req.url === "/sql") {
-                req.on("end", () => {
+                }
+            } else if (req.method === "GET") {
+                if (req.url === "/sql") {
                     const query = JSON.parse(body).query;
                     if (/^(SELECT)/i.test(query)) {
                         database.query(query, (err, results) => {
@@ -54,9 +66,12 @@ class API {
                         res.writeHead(400, { "Content-Type": "application/json" });
                         res.end(JSON.stringify({ error: "Only SELECT queries are allowed in GET requests." }));
                     }
-                });
+                }
+            } else {
+                res.writeHead(404, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Not Found" }));
             }
-        }
+        });
     }
 
     listen(port = 8080) {
