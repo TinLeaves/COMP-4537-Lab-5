2f2f7d5121d50a9550fcd726be78c9255d7baefe
qthompson2 <q.thompson088@gmail.com>
2024-10-13 23:10:34 -0700

    Update db.js

--- server 2/db.js
+++ server 2/db.js
@@ -8,6 +8,7 @@ class Database {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
     });
 
     console.log(process.env.DB_HOST);
