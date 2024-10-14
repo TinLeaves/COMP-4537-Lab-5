b7ea1dc4c345c5cdfe33fde24921878acda72d80
qthompson2 <q.thompson088@gmail.com>
2024-10-13 23:08:37 -0700

    removes ssl code

--- server 2/db.js
+++ server 2/db.js
@@ -8,9 +8,6 @@ class Database {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      ssl: {
-        ca: process.env.DB_CA
-      }
     });
 
     console.log(process.env.DB_HOST);
