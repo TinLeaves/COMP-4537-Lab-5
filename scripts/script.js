d7266d1486d7d6188e2a0097e7164380922ce9af
qthompson2 <q.thompson088@gmail.com>
2024-10-13 18:36:34 -0700

    Makes front-end js object oriented

--- scripts/script.js
+++ scripts/script.js
@@ -3,31 +3,42 @@ const queryInput = document.getElementById('queryInput');
 const executeQueryButton = document.getElementById('executeQueryButton');
 const responseOutput = document.getElementById('responseOutput');
 
-insertButton.addEventListener('click', () => {
-    fetch('/insert', { method: 'POST' })
-        .then(response => response.text())
-        .then(data => {
-            responseOutput.textContent = data;
-        })
-        .catch(error => {
-            responseOutput.textContent = 'Error: ' + error;
-        });
-});
+class UserInterface {
+    constructor() {
+        this.insertButton = document.getElementById('insertButton');
+        this.queryInput = document.getElementById('queryInput');
+        this.executeQueryButton = document.getElementById('executeQueryButton');
+        this.responseOutput = document.getElementById('responseOutput');
+    }
 
-executeQueryButton.addEventListener('click', () => {
-    const query = queryInput.value;
-    fetch('/sql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query })
-    })
-        .then(response => response.json())
-        .then(data => {
-            responseOutput.textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            responseOutput.textContent = 'Error: ' + error;
+    addListerners() {
+        this.insertButton.addEventListener('click', () => {
+            fetch('/insert', { method: 'POST' })
+                .then(response => response.text())
+                .then(data => {
+                    this.responseOutput.textContent = data;
+                })
+                .catch(error => {
+                    this.responseOutput.textContent = 'Error: ' + error;
+                });
         });
-});
+
+        this.executeQueryButton.addEventListener('click', () => {
+            const query = this.queryInput.value;
+            fetch('/sql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ query })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    this.responseOutput.textContent = JSON.stringify(data, null, 2);
+                })
+                .catch(error => {
+                    this.responseOutput.textContent = 'Error: ' + error;
+                });
+        });
+    }
+}
