6fa43e9181c8211c0ec0bb7e94346ab237fe356e
qthompson2 <q.thompson088@gmail.com>
2024-10-15 11:25:21 -0700

    Updates server.js to create the table if it doesn't exist

--- server 2/server.js
+++ server 2/server.js
@@ -18,84 +18,90 @@ class API {
 
     handleRequest(req, res) {
         this.setCorsHeaders(res);
+
+        database.checkTableExists().then((exists) => {
+            if (!exists) {
+                database.createTable();
+            }
     
-        if (req.method === "OPTIONS") {
-            res.writeHead(204);
-            res.end();
-            return;
-        }
+            if (req.method === "OPTIONS") {
+                res.writeHead(204);
+                res.end();
+                return;
+            }
+            
+            //From https://stackoverflow.com/questions/31006711/get-request-body-from-node-jss-http-incomingmessage
+            let body = "";
+            req.on("data", (chunk) => {
+                body += chunk.toString();
+            });
         
-        //From https://stackoverflow.com/questions/31006711/get-request-body-from-node-jss-http-incomingmessage
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk.toString();
-        });
-    
-        req.on("end", () => {
-            console.log(`Received ${req.method} request for ${req.url}`);
-    
-            if (req.method === "POST") {
-                if (req.url === "/insertTestRows") {
-                    console.log("Inserting test rows...");
-                    database.insertTestRows(res);
-                } else if (req.url === "/sql") {
-                    try {
-                        const query = JSON.parse(body).query;
-                        if (/^(INSERT)/i.test(query)) {
-                            database.db.execute(query)
-                                .then((results) => {
-                                    res.writeHead(200, { "Content-Type": "application/json" });
-                                    res.end(JSON.stringify(results));
-                                })
-                                .catch((err) => {
-                                    res.writeHead(500, { "Content-Type": "application/json" });
-                                    res.end(JSON.stringify({ error: err.message }));
-                                });
-                        } else {
+            req.on("end", () => {
+                console.log(`Received ${req.method} request for ${req.url}`);
+        
+                if (req.method === "POST") {
+                    if (req.url === "/insertTestRows") {
+                        console.log("Inserting test rows...");
+                        database.insertTestRows(res);
+                    } else if (req.url === "/sql") {
+                        try {
+                            const query = JSON.parse(body).query;
+                            if (/^(INSERT)/i.test(query)) {
+                                database.db.execute(query)
+                                    .then((results) => {
+                                        res.writeHead(200, { "Content-Type": "application/json" });
+                                        res.end(JSON.stringify(results));
+                                    })
+                                    .catch((err) => {
+                                        res.writeHead(500, { "Content-Type": "application/json" });
+                                        res.end(JSON.stringify({ error: err.message }));
+                                    });
+                            } else {
+                                res.writeHead(400, { "Content-Type": "application/json" });
+                                res.end(JSON.stringify({ error: "Only INSERT queries are allowed in POST requests." }));
+                            }
+                        } catch (error) {
                             res.writeHead(400, { "Content-Type": "application/json" });
-                            res.end(JSON.stringify({ error: "Only INSERT queries are allowed in POST requests." }));
+                            res.end(JSON.stringify({ error: "Invalid JSON format." }));
                         }
-                    } catch (error) {
-                        res.writeHead(400, { "Content-Type": "application/json" });
-                        res.end(JSON.stringify({ error: "Invalid JSON format." }));
                     }
-                }
-            } else if (req.method === "GET") {
-                let url = new URL(req.url, `http://${req.headers.host}`);
-                let query = url.searchParams.get("query");
-    
-                console.log(`Pathname: ${url.pathname}`);
-                console.log(`Received query: ${query}`);
-    
-                if (url.pathname === "/sql") {
-                    console.log("Handling GET request for /sql");
-                    try {
-                        if (/^(SELECT)/i.test(query)) {
-                            database.db.execute(query)
-                                .then((results) => {
-                                    res.writeHead(200, { "Content-Type": "application/json" });
-                                    res.end(JSON.stringify(results));
-                                })
-                                .catch((err) => {
-                                    res.writeHead(500, { "Content-Type": "application/json" });
-                                    res.end(JSON.stringify({ error: err.message }));
-                                });
-                        } else {
+                } else if (req.method === "GET") {
+                    let url = new URL(req.url, `http://${req.headers.host}`);
+                    let query = url.searchParams.get("query");
+        
+                    console.log(`Pathname: ${url.pathname}`);
+                    console.log(`Received query: ${query}`);
+        
+                    if (url.pathname === "/sql") {
+                        console.log("Handling GET request for /sql");
+                        try {
+                            if (/^(SELECT)/i.test(query)) {
+                                database.db.execute(query)
+                                    .then((results) => {
+                                        res.writeHead(200, { "Content-Type": "application/json" });
+                                        res.end(JSON.stringify(results));
+                                    })
+                                    .catch((err) => {
+                                        res.writeHead(500, { "Content-Type": "application/json" });
+                                        res.end(JSON.stringify({ error: err.message }));
+                                    });
+                            } else {
+                                res.writeHead(400, { "Content-Type": "application/json" });
+                                res.end(JSON.stringify({ error: "Only SELECT queries are allowed in GET requests." }));
+                            }
+                        } catch (error) {
                             res.writeHead(400, { "Content-Type": "application/json" });
-                            res.end(JSON.stringify({ error: "Only SELECT queries are allowed in GET requests." }));
+                            res.end(JSON.stringify({ error: "Invalid Search Parameter format." }));
                         }
-                    } catch (error) {
-                        res.writeHead(400, { "Content-Type": "application/json" });
-                        res.end(JSON.stringify({ error: "Invalid Search Parameter format." }));
+                    } else {
+                        res.writeHead(404, { "Content-Type": "application/json" });
+                        res.end(JSON.stringify({ error: "Not Found" }));
                     }
                 } else {
-                    res.writeHead(404, { "Content-Type": "application/json" });
-                    res.end(JSON.stringify({ error: "Not Found" }));
+                    res.writeHead(405, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ error: "Method Not Allowed" }));
                 }
-            } else {
-                res.writeHead(405, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ error: "Method Not Allowed" }));
-            }
+            });
         });
     }    
 
